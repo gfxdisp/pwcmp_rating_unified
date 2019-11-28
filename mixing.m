8215d7434de0d1141481c7f20a921293c8bd0d48
Aliaksei Mikhailiuk <am2442@cl.cam.ac.uk>
2019-11-28 15:32:06 +0000

    updated optimisation

--- mixing.m
+++ mixing.m
@@ -45,7 +45,7 @@ end
 
 numb_datasets = numel(datasets_sizes);
 
-options = optimset( 'Display', 'off', 'LargeScale', 'off', 'MaxIter', 5000 );
+options = optimset( 'Display', 'off', 'LargeScale', 'off', 'MaxIter', 10000 );
 
 % Find non-zero elements in matrix D+D', i.e. compared conditions
 Dt = D';
